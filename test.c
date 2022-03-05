2f412c7b9917520d916c5956021575d8f6003c25
wangyixu12 <wangyixu12@gmail.com>
2022-03-05 10:10:04 +0800

    test

---
+++ test.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: Xu Wang
+ * @Date: 2022-03-05 10:08:01
+ * @LastEditors: Xu Wang
+ * @LastEditTime: 2022-03-05 10:08:19
+ * @Description: 
+ */
+#include <stdio.h>
+int main(void)
+{
+    printf("test\n");
+    return 0;
+}
